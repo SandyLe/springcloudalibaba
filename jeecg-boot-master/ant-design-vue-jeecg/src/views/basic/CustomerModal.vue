515744250de4a748ac2df665ed67ed5713db4de4
SandyLe <li_xianting@163.com>
2019-11-28 23:14:12 +0800

    客户相关

---
+++ jeecg-boot-master/ant-design-vue-jeecg/src/views/basic/CustomerModal.vue
@@ -0,0 +1,164 @@
+<template>
+  <a-modal
+    :title="title"
+    :width="800"
+    :visible="visible"
+    :confirmLoading="confirmLoading"
+    @ok="handleOk"
+    @cancel="handleCancel"
+    cancelText="关闭"
+    wrapClassName="ant-modal-cust-warp"
+    style="top:5%;height: 65%;overflow-y: hidden">
+    <a-spin :spinning="confirmLoading">
+      <a-form :form="form">
+
+        <a-row>
+          <a-col :md="12" :sm="12">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="名称">
+              <a-input placeholder="请输入名称" v-decorator="[ 'name', validatorRules.name]" />
+            </a-form-item>
+          </a-col>
+          <a-col :md="12" :sm="12">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="代码">
+              <a-input placeholder="请输入代码" v-decorator="[ 'code', validatorRules.code]" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :md="24" :sm="24">
+            <a-form-item
+              :labelCol="hlabelCol"
+              :wrapperCol="hwrapperCol"
+              label="是否启用分类价格"
+              label-width="4">
+              <j-dict-select-tag v-decorator="['priceFlag', {}]" placeholder="是否启用分类价格" :type="'radio'" :triggerChange="true" dictCode="price_flag"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :md="24" :sm="24">
+            <a-form-item
+              :labelCol="hlabelCol"
+              :wrapperCol="hwrapperCol"
+              label="备注"
+              label-width="4">
+              <a-input placeholder="请输入备注" v-decorator="[ 'content', validatorRules.content]" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+    </a-spin>
+  </a-modal>
+</template>
+
+<script>
+  import pick from 'lodash.pick'
+  import AFormItem from "ant-design-vue/es/form/FormItem";
+  import JDictSelectTag from '@/components/dict/JDictSelectTag.vue'
+  import {addCustomerType,editCustomerType,duplicateCheck } from '@/api/api'
+  export default {
+    name: "CustomerTypeModal",
+    data() {
+      return {
+        title: "操作",
+        visible: false,
+        roleDisabled: false,
+        confirmLoading: false,
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 8 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        hlabelCol: {
+          xs: { span: 24 },
+          sm: { span: 4 },
+        },
+        hwrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        model: {},
+        form: this.$form.createForm(this),
+        validatorRules: {
+          name: {
+            rules: [
+              { min: 0, max: 126, message: '长度不超过 126 个字符', trigger: 'blur' }
+            ]
+          }
+        }
+      }
+    },
+    components: {AFormItem,JDictSelectTag},
+    created () {
+    },
+    methods: {
+      add () {
+        this.edit({priceFlag:'0'});
+      },
+      edit (record) {
+        this.form.resetFields();
+        this.model = Object.assign({}, record);
+        this.visible = true;
+
+        //编辑页面禁止修改角色编码
+        if(this.model.id){
+          this.roleDisabled = true;
+        }else{
+          this.roleDisabled = false;
+        }
+
+        this.$nextTick(() => {
+          this.form.setFieldsValue(pick(this.model,'name', 'code','content','priceFlag'))
+        });
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        const that = this;
+        // 触发表单验证
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            that.confirmLoading = true;
+            let formData = Object.assign(this.model, values);
+            let obj;
+            console.log(formData)
+            if(!this.model.id){
+              obj=addCustomerType(formData);
+            }else{
+              obj=editCustomerType(formData);
+            }
+            obj.then((res)=>{
+              if(res.success){
+                that.$message.success(res.message);
+                that.$emit('ok');
+              }else{
+                that.$message.warning(res.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+              that.close();
+            })
+          }
+        })
+      },
+      handleCancel () {
+        this.close()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
