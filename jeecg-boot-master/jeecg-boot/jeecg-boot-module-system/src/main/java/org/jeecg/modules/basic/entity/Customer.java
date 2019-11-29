d720b5f78918e3e35815c3148fa856e5f69d3b4c
lixianting <li_xianting@163.com>
2019-11-29 18:25:50 +0800

    Default Changelist

---
+++ jeecg-boot-master/jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/basic/entity/Customer.java
@@ -0,0 +1,260 @@
+package org.jeecg.modules.basic.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@TableName("sl_customer")
+@ApiModel(value = "Customer", description = "客户")
+public class Customer extends BasicEntity {
+
+    @ApiModelProperty("客户类型")
+    private String customerTypeId;
+    @TableField(exist=false)
+    private String customerType;
+    private String phone;
+    private String wechatId;
+    private String wechatNickName;
+    private String customerSourceId;
+    private String customerSource;
+    private String memberNo;
+    private String memberPhone;
+    private String memberNickName;
+    private String nickName;
+    private Integer gender;
+    private Date birthday;
+    private String linkman;
+    private String tel;
+    private String email;
+    private String fax;
+    private String bankaccount;
+    private String bankacctName;
+    private String bankName;
+    private BigDecimal discount;
+    private String discountTypeId;
+    private String province;
+    private String city;
+    private String district;
+    private String address;
+    private String billingInfo;
+
+    public String getCustomerTypeId() {
+        return customerTypeId;
+    }
+
+    public void setCustomerTypeId(String customerTypeId) {
+        this.customerTypeId = customerTypeId;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWechatId() {
+        return wechatId;
+    }
+
+    public void setWechatId(String wechatId) {
+        this.wechatId = wechatId;
+    }
+
+    public String getWechatNickName() {
+        return wechatNickName;
+    }
+
+    public void setWechatNickName(String wechatNickName) {
+        this.wechatNickName = wechatNickName;
+    }
+
+    public String getCustomerSourceId() {
+        return customerSourceId;
+    }
+
+    public void setCustomerSourceId(String customerSourceId) {
+        this.customerSourceId = customerSourceId;
+    }
+
+    public String getCustomerSource() {
+        return customerSource;
+    }
+
+    public void setCustomerSource(String customerSource) {
+        this.customerSource = customerSource;
+    }
+
+    public String getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(String memberNo) {
+        this.memberNo = memberNo;
+    }
+
+    public String getMemberPhone() {
+        return memberPhone;
+    }
+
+    public void setMemberPhone(String memberPhone) {
+        this.memberPhone = memberPhone;
+    }
+
+    public String getMemberNickName() {
+        return memberNickName;
+    }
+
+    public void setMemberNickName(String memberNickName) {
+        this.memberNickName = memberNickName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getLinkman() {
+        return linkman;
+    }
+
+    public void setLinkman(String linkman) {
+        this.linkman = linkman;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getBankaccount() {
+        return bankaccount;
+    }
+
+    public void setBankaccount(String bankaccount) {
+        this.bankaccount = bankaccount;
+    }
+
+    public String getBankacctName() {
+        return bankacctName;
+    }
+
+    public void setBankacctName(String bankacctName) {
+        this.bankacctName = bankacctName;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getDiscountTypeId() {
+        return discountTypeId;
+    }
+
+    public void setDiscountTypeId(String discountTypeId) {
+        this.discountTypeId = discountTypeId;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBillingInfo() {
+        return billingInfo;
+    }
+
+    public void setBillingInfo(String billingInfo) {
+        this.billingInfo = billingInfo;
+    }
+}
