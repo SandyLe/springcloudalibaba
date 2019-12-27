cdbf65416b5eb745133c0e3c47c8b07a750f1be7
tomkluas <1175704775@qq.com>
2019-12-27 17:44:33 +0800

    采购列表功功能调整（之前的结构不正确）

---
+++ jeecg-boot-master/jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/purchase/service/IPurchaseService.java
@@ -0,0 +1,13 @@
+package org.jeecg.modules.purchase.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.jeecg.modules.purchase.entity.Purchase;
+
+/**
+ * @Description:
+ * @Author: tomkluas
+ * @Date: 2019/12/27 16:13
+ * @Version: V1.0
+ */
+public interface IPurchaseService extends IService<Purchase> {
+}
