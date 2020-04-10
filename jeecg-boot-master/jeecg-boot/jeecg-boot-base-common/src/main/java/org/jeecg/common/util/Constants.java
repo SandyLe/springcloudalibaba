8a0844307f689480a52be77399dbc7498bcc4ab8
SandyLe <li_xianting@163.com>
2020-04-11 01:28:58 +0800

    包结构调整

--- jeecg-boot-master/jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/util/Constants.java
+++ jeecg-boot-master/jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/util/Constants.java
@@ -1,5 +1,11 @@
 package org.jeecg.common.util;
 
+import org.jeecg.common.enums.BillType;
+import org.jeecg.common.enums.InventoryOperation;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 常量类
  */
@@ -13,4 +19,21 @@ public class Constants {
     public static final String CHARSET_UTF8 = "utf-8";
     //gzip
     public static final String CONTENT_ENCODING_GZIP = "gzip";
+
+    public static final Map<Integer, Integer> OUTOPERATIONS = new HashMap<Integer, Integer>();
+    public static final Map<Integer, Integer> INOPERATIONS = new HashMap<Integer, Integer>();
+
+    static {
+        OUTOPERATIONS.put(BillType.SALEORDER.getId(), InventoryOperation.SALEOUT.getId());
+        OUTOPERATIONS.put(BillType.PURCHASERETURNORDER.getId(), InventoryOperation.PURCHASERETURNOUT.getId());
+        OUTOPERATIONS.put(BillType.ALLOT.getId(), InventoryOperation.ALLOTOUT.getId());
+        OUTOPERATIONS.put(BillType.ASSEMBLE.getId(), InventoryOperation.ASSEMBLEOUT.getId());
+        OUTOPERATIONS.put(BillType.TEARDOWN.getId(), InventoryOperation.TEARDOWNOUT.getId());
+
+        INOPERATIONS.put(BillType.SALERETURNORDER.getId(), InventoryOperation.SALERETURNIN.getId());
+        INOPERATIONS.put(BillType.PURCHASEORDER.getId(), InventoryOperation.PURCHASEIN.getId());
+        INOPERATIONS.put(BillType.ALLOT.getId(), InventoryOperation.ALLOTIN.getId());
+        INOPERATIONS.put(BillType.ASSEMBLE.getId(), InventoryOperation.ASSEMBLEIN.getId());
+        INOPERATIONS.put(BillType.TEARDOWN.getId(), InventoryOperation.TEARDOWNIN.getId());
+    }
 }
